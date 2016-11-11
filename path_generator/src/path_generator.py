eb41471eba2ff4354c64b3960293afa23bc50ea7
Nino Cauli <nigno17@gmail.com>
2016-11-11 12:46:12 +0000

    added the timestamp to the path generator

--- path_generator/src/path_generator.py
+++ path_generator/src/path_generator.py
@@ -14,24 +14,26 @@ def talker():
     rospy.init_node('path_generator', anonymous=True)
     
     freq = 100;
-    round_min = 0.25;
+    round_min = 0.1;
     
     path_msg = Path();
+    path_msg.header.stamp = rospy.get_rostime();
     path_msg.poses = [];
     path_msg.header.frame_id = "nav";
     
-    radius = 1.0;    
+    radius = 2.0;    
     rate = rospy.Rate(freq) # 100hz
 
     count = 0;
     while not rospy.is_shutdown():
         count += 1;
         pos_msg = PoseStamped();
+        pos_msg.header.stamp = rospy.get_rostime();
         pos_msg.header.frame_id = "nav";
         
         pos_msg.pose.position.x = radius * np.cos((np.pi * 2) / freq * round_min * count);
         pos_msg.pose.position.y = radius * np.sin((np.pi * 2) / freq * round_min * count);
-        pos_msg.pose.position.z = 1.0;       
+        pos_msg.pose.position.z = 1.5;       
         
         if count > (freq / round_min):
             path_msg.poses.pop(0)
